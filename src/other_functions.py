05c250457398972d8959c36505ff84d39651465d
renebidart <renebidart@gmail.com>
2018-04-27 11:27:55 -0400

    whatever I forgot

--- src/other_functions.py
+++ src/other_functions.py
@@ -79,54 +79,6 @@ def bagging_ensemble_acc(models_arch, models_name, PATH, train_folder, csv_fname
     print(accuracy_np(probs, all_y))
     return int_acc
 
-def bagging_ensemble_acc_dir(models_arch, models_name, models_path, PATH, weights=None):
-    """ Test list of pre-trained fastai models using the keras-style paths 
-
-    models_path - standard fastai format directory for models
-    PATH - path to dataset
-    model_arch - list of models
-    models_name - list of names of models weights
-    Model parameters (models_name) must be in same order as models_arch
-    Weights is used for final ensemble accuracy
-    """
-    if weights is None:
-        weights = [1/len(models_arch)]*len(models_arch)
-    all_preds = []
-    all_y = []
-    int_acc_list = []
-    
-    for idx, arch in enumerate(models_arch):
-        if arch in [inceptionresnet_2, inception_4]:
-            sz = 299
-        else:
-            sz = 224
-
-        tfms = tfms_from_model(arch, sz, aug_tfms=None, max_zoom=1)
-        
-        # do data from path, because this is the same split, just doesn't req
-        # get the validation indices based on this
-        data = ImageClassifierData.from_paths(PATH, tfms=tfms, bs=64)
-
-        model_loc = os.path.join(models_path, 'models', models_name[idx])
-        learn = ConvLearner.pretrained(arch, data, precompute=False)
-        learn.load(model_loc)
-        # this returns log probs, but don't need to do anything because just taking the argmax
-        preds, y = learn.predict_with_targs(is_test=False)
-        print(models_name[idx], ':   ', accuracy_np(preds, y))
-        all_preds.append(preds)
-        all_y.append(y)
-
-        curr_preds = np.array(all_preds)
-        curr_probs = np.average(np.exp(curr_preds), axis=0)
-        int_acc = accuracy_np(curr_probs, y)
-        int_acc_list.append(int_acc)
-        print(models_name[idx], 'int acc:   ', int_acc)
-
-    all_preds = np.array(all_preds)
-    probs = np.average(np.exp(all_preds), axis=0, weights=weights)
-    print(accuracy_np(probs, all_y))
-    return int_acc_list
-
 
 def make_validation_mask(data_df, normal_valid, tumor_valid):
     img_names = data_df['file_name'].tolist()
@@ -166,3 +118,48 @@ def get_best_ensemble_acc(models, models_name, PATH):
         probs = np.average(np.exp(all_preds), axis=0, weights=weights)
         print('Power:', power, 'Accuracy: ', accuracy_np(probs, y))
     return perf
+
+
+def ensemble_acc_dir(models_arch, models_name, models_path, PATH, weights=None):
+    # weights must be in the same order corresponding to model
+    # weights only used for the final ensemble, not the intermediate ones
+    if weights is None:
+        weights = [1/len(models_arch)]*len(models_arch)
+    all_preds = []
+    all_y = []
+    acc_list = []
+    int_acc_list = []
+    
+    for idx, arch in enumerate(models_arch):
+        if arch in [inceptionresnet_2, inception_4]:
+            sz = 299
+        else:
+            sz = 224
+
+        tfms = tfms_from_model(arch, sz, aug_tfms=None, max_zoom=1)
+        
+        # do data from path, because this is the same split, just doesn't req
+        # get the validation indices based on this
+        data = ImageClassifierData.from_paths(PATH, tfms=tfms, bs=64)
+
+        model_loc = os.path.join(models_path, 'models', models_name[idx])
+        learn = ConvLearner.pretrained(arch, data, precompute=False)
+        learn.load(model_loc)
+        # this returns log probs, but don't need to do anything because just taking the argmax
+        preds, y = learn.predict_with_targs(is_test=False)
+        print(models_name[idx], ':   ', accuracy_np(preds, y))
+        acc_list.append(accuracy_np(preds, y))
+        all_preds.append(preds)
+        all_y.append(y)
+
+        curr_preds = np.array(all_preds)
+        curr_probs = np.average(np.exp(curr_preds), axis=0)
+        int_acc = accuracy_np(curr_probs, y)
+        int_acc_list.append(int_acc)
+        # print(models_name[idx], 'int acc:   ', int_acc)
+
+    all_preds = np.array(all_preds)
+    probs = np.average(np.exp(all_preds), axis=0, weights=weights)
+    bagging_acc = accuracy_np(probs, all_y)
+    print(bagging_acc)
+    return bagging_acc, acc_list, int_acc_list
